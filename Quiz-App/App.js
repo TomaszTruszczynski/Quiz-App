609a8be840f727209a68493e4160d131f36671a1
OskarPodgorski <tomtru97@gmail.com>
2025-06-08 04:40:29 +0200

    added counter button

--- Quiz-App/App.js
+++ Quiz-App/App.js
@@ -1,20 +1,22 @@
+import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, } from 'react-native';
 
 export default function App() {
+  const [counter, setCounter] = useState(0);
+
+  function countUp() {
+    setCounter(c => c + 1);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ backgroundColor: "grey", flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <StatusBar style="dark" />
+      <Text>{counter}</Text>
+      <TouchableOpacity style={{ backgroundColor: "orange", padding: 50, borderRadius: 10, elevation: 10, position: "absolute", bottom: 0, left: 0 }}
+        onPress={countUp}>
+        <Text>Przycisk</Text>
+      </TouchableOpacity>
     </View>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+};
